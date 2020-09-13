934509d8cdb93c5a8b236ee09f3929936a83e5ce
baudin999 <ckelkboom@protonmail.com>
2020-09-13 09:40:58 +0200

    Working on setting up the project, init tests

---
+++ tests/lexer.test.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { it } from "mocha";
+
+
+describe('lexer', function () {
+    it('add', function () {
+        let result = 2;
+        expect(result).equal(2);
+    });
+});
